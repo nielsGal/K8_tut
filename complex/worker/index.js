03aa76e3f157ec97ebfa6fae822c54090fc3ef3c
nielsgal <galjaardniels@gmail.com>
2020-07-18 10:05:48 +0200

    worker

---
+++ complex/worker/index.js
@@ -0,0 +1,15 @@
+const key = require("./keys");
+const redis = require("redis");
+const keys = require("./keys");
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
+});
+const sub = redisClient.duplicate();
+
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
+}

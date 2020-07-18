1fdba678612486a05c54882056393ae2a17e4b3e
nielsgal <galjaardniels@gmail.com>
2020-07-18 14:03:46 +0200

    docker compose bare bones

--- complex/server/index.js
+++ complex/server/index.js
@@ -16,11 +16,11 @@ const pgClient = new Pool({
   port: keys.pgPort,
 });
 
-pgClient.on("error", () => console.log("Lost PG connection"));
-
-pgClient
-  .query("CREATE TABLE IF NOT EXISTS values (number INT)")
-  .catch((err) => console.log(err));
+pgClient.on("connect", () => {
+  pgClient
+    .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+    .catch((err) => console.log(err));
+});
 
 const reids = require("redis");
 const { reset } = require("nodemon");
